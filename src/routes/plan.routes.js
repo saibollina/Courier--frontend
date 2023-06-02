15c1f071dba8cc6d63bcf195a353705c698cad26
saibollina <127999605+saibollina@users.noreply.github.com>
2023-06-02 02:32:59 -0500

    getPlan changes

--- src/routes/plan.routes.js
+++ src/routes/plan.routes.js
@@ -6,7 +6,7 @@ export const planRouter = Router();
 
 planRouter.get('/plans', getAllplans);
 planRouter.get("/plans/search", searchPlan);
-planRouter.get('/plans/:placeId', getplan);
+planRouter.get('/plans/:planId', getplan);
 planRouter.post('/place',[bodyParser.urlencoded({ extended: false }),bodyParser.json()],createPlan)
 planRouter.delete("/plans/:id", deletePlan);
 planRouter.delete("/plans/", deleteAllPlans);
