d2fb7ebd9d4e240c4420be11be13028153935968
Vasanth Kumar Thejavath <117776118+VasanthKumar3256@users.noreply.github.com>
2023-06-12 15:25:07 -0500

    changes in plan routes

--- src/routes/plan.routes.js
+++ src/routes/plan.routes.js
@@ -7,7 +7,7 @@ export const planRouter = Router();
 planRouter.get('/plans', getAllplans);
 planRouter.get("/plans/search", searchPlan);
 planRouter.get('/plans/:planId', getplan);
-planRouter.post('/place',[bodyParser.urlencoded({ extended: false }),bodyParser.json()],createPlan)
+planRouter.post('/plans',[bodyParser.urlencoded({ extended: false }),bodyParser.json()],createPlan)
 planRouter.delete("/plans/:id", deletePlan);
 planRouter.delete("/plans/", deleteAllPlans);
 planRouter.put("/plans/:id", updatePlan);
