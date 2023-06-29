fdd47db02b8e3493f2c2d355bf5da797c30841fd
shrikanth01 <132511291+shrikanth01@users.noreply.github.com>
2023-06-29 16:10:21 -0500

    order details changes

--- src/controller/order.controller.js
+++ src/controller/order.controller.js
@@ -1,7 +1,6 @@
 import db from "../models/index.js";
-import { encrypt, getSalt, hashPassword } from "../authentication/crypto.js";
 const Order = db.order;
-const Session = db.session;
+const OrderDetails = db.orderDetails
 const Op = db.Sequelize.Op;
 
 // Create and Save a new Order
@@ -11,8 +10,24 @@ export const create = async (req, res) => {
     const error = new Error("coustomerID cannot be empty for order!");
     error.statusCode = 400;
     throw error;
-  } else if (req.body.orderedBy === undefined) {
+  }
+  if (req.body.orderedBy === undefined) {
     const error = new Error("orderedBy cannot be empty for order!");
+    error.statusCode = 400;
+    throw error;
+  }
+  if (req.body.dropLocation === undefined) {
+    const error = new Error("droplocation cannot be empty for order!");
+    error.statusCode = 400;
+    throw error;
+  }
+  if (req.body.pickedUpLocation === undefined) {
+    const error = new Error("pickup location cannot be empty for order!");
+    error.statusCode = 400;
+    throw error;
+  }
+  if (req.body.cost === undefined) {
+    const error = new Error("cost cannot be empty for order!");
     error.statusCode = 400;
     throw error;
   }
@@ -21,18 +36,52 @@ export const create = async (req, res) => {
     orderedBy :req.body.orderedBy,
     orderPlacedTime: new Date(),
   }
-  // find by email
-  Order.create(order).then((data)=>{
-    res.send(data)
+  let orderId = null;
+  await Order.create(order).then((data)=>{
+    console.log('orderPlaced', data)
+    orderId= data.dataValues.id
   }).catch((error)=> {
     console.log({error});
     res.status(500).send({
         message: "Cannot place order now",
       });
-  })
+  });
+  if( orderId){
+    const orderDetails= {
+      orderId:orderId,
+      cost: req.body.cost,
+      pickedUpLocation:req.body.pickedUpLocation,
+      dropLocation:req.body.dropLocation,
+      status:'Order-placed'
+    }
+    await OrderDetails.create(orderDetails).then((data)=> res.send(data))
+    .catch((error)=>{
+      console.log('deleting half baked order with orderID', orderId, error);
+      Order.destroy({
+        where: { id: orderId },
+      })
+        .then((number) => {
+          if (number == 1) {
+            res.send({
+              message: "Order was deleted successfully!",
+            });
+          } else {
+            res.send({
+              message: `Cannot delete orderId with id = ${id}. Maybe Order was not found!`,
+            });
+          }
+        })
+        .catch((err) => {
+          res.status(500).send({
+            message: err.message || "Could not delete Order with id = " + id,
+          });
+        });
+    })
+  }
+
 };
 
-// Retrieve all Users from the database.
+
 export const findAll = (req, res) => {
   const id = req.query.id;
   var condition = id ? { id: { [Op.like]: `%${id}%` } } : null;
